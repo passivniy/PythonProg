092bef0ad8c2166f956931743fc8a5361536bb3b
passivniy <danik133743@gmail.com>
2023-05-10 08:56:52 +0400

    Add func: validate_zero_division()

--- Lytvynov_Task2.py
+++ Lytvynov_Task2.py
@@ -10,6 +10,12 @@ def set_values():
 			print('Uncorrect value.')
 
 
-x, y, z = set_values()
+def validate_zero_division():
+	while True:
+			x, y, z = set_values()
+			if (18.2 - pow(3.8, y) + 19.3)!=0:
+				return x,y,z
 
+
+x, y, z = validate_zero_division()
 print((x + z / 8) / (18.2 - pow(3.8, y) + 19.3))
