9543ffee38c183a44bc7b89c1750ec8a3d6f6d9e
passivniy <danik133743@gmail.com>
2023-05-10 08:20:22 +0400

    Add print lines

--- Lytvynov_Task1.py
+++ Lytvynov_Task1.py
@@ -1,9 +1,9 @@
-print('\'IKM-221D\':Task 1')
-print('Lytvynov Danyil')
-for i in range(45):
-    if i == 44:
-        print('Владислав Валериевич.')
-        break
-    print('Владислав Валериевич, ', end='')
+print('''IKM-221D:Task 1
+Lytvynov Danyil''')
 
-print('#11', round((195+1.3/8)/(18.2-(pow(3.8, 2))+19.3), 2))
+teacher_name='Владислав Валериевич'
+#print(teacher_name[:-2])
+names = [teacher_name for x in range(45)]
+print(', '.join(names))
+
+print(f'#11 {round((195 + 1.3 / 8) / (18.2 - (pow(3.8, 2)) + 19.3), 2)}')
