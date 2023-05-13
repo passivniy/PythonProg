2699d152eed4fae5af3e0995c160e95aee7fe3cc
passivniy <danik133743@gmail.com>
2023-05-13 13:23:36 +0400

    Add module with func numbers_isnt_negative

--- Lytvynov_Task3.py
+++ Lytvynov_Task3.py
@@ -1,6 +1,21 @@
 import math
-# IKM-221D,Task_3,Lytvynov Danyil
-print('Task_3')
-print('Danyil Lytvynov', 'IKM-221D')
-x = int(input())
-print('y=', math.sqrt(math.log(pow(x, 2)-4)))
+from import_modules.validator import set_values
+from import_modules.isnt_negatives import numbers_isnt_negatives
+
+
+# MODULE isnt_negatives: 
+
+# def numbers_isnt_negatives(lst:list):
+#    for x,i in enumerate(lst):
+#        while lst[x]<=0:
+#            lst[x]=int(input(f'Enter correct value(i>0,your value is : {lst[x]}): '))
+#   return lst if len(lst)>1 else lst[0]
+
+
+
+print('''Task_3 Danyil
+ Lytvynov IKM-221D Task_3 ''')
+
+x=numbers_isnt_negatives(set_values())
+
+print(f'y = {math.sqrt(math.log(pow(x, 2) - 4))}')
