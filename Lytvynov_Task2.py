55209dd669b771aae30dd90fc9ef555a6bef3f25
passivniy <danik133743@gmail.com>
2023-05-20 13:34:09 +0200

    Fix code style Task-2

--- Lytvynov_Task2.py
+++ Lytvynov_Task2.py
@@ -2,11 +2,12 @@ from import_modules.validator import set_values
 print('''Danyil Lytvynov
 Task_2 IKM-221D''')
 
+
 def validate_zero_division():
 	while True:
-			x, y, z = set_values()
-			if (18.2 - pow(3.8, y) + 19.3)!=0:
-				return x,y,z
+		x, y, z = set_values()
+		if (18.2 - pow(3.8, y) + 19.3) != 0:
+			return x, y, z
 
 
 x, y, z = validate_zero_division()
