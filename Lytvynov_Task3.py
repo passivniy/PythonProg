e49c0bb9ba76ca2f08a27a2e0c897a3f79d06301
passivniy <danik133743@gmail.com>
2023-05-20 13:46:49 +0200

    Fix code style Task-3

--- Lytvynov_Task3.py
+++ Lytvynov_Task3.py
@@ -1,21 +1,9 @@
 import math
-from import_modules.validator import set_values
-from import_modules.isnt_negatives import numbers_isnt_negatives
-
-
-# MODULE isnt_negatives: 
-
-# def numbers_isnt_negatives(lst:list):
-#    for x,i in enumerate(lst):
-#        while lst[x]<=0:
-#            lst[x]=int(input(f'Enter correct value(i>0,your value is : {lst[x]}): '))
-#   return lst if len(lst)>1 else lst[0]
-
-
+from import_modules.validator import numbers_isnt_negatives
 
 print('''Task_3 Danyil
  Lytvynov IKM-221D Task_3 ''')
 
-x=numbers_isnt_negatives(set_values())
+x = numbers_isnt_negatives()
 
 print(f'y = {math.sqrt(math.log(pow(x, 2) - 4))}')
