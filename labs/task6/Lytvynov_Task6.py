991f4777929e58dd352dd39e7efe394b79fc2c07
passivniy <danik133743@gmail.com>
2023-05-27 21:13:09 +0200

    Fix style code

--- labs/task6/Lytvynov_Task6.py
+++ labs/task6/Lytvynov_Task6.py
@@ -5,23 +5,24 @@ print('IKM-221D Task_6')
 print('Lytvynov Danyil')
 
 
+#Цикл в комрехеншен [], открытие в 1 файл
 def read_and_write(inp_filename: str, out_filename: str):
     summ = int()
     with open(inp_filename, 'r') as inp_param:
         for line in inp_param:
             summ += int(line)
+
     with open(out_filename, 'w') as out_summ:
         out_summ.write(str(summ))
-    return 'Completed.'
 
 
 def even_or_odd(out_filename: str):
     x = set_value(int)
     with open(out_filename, 'w') as out:
-        out.write('Even' if x % 2 == 0 else 'Odd')
-    return 'Completed'
+        out.write('Odd' if x % 2 else 'Even')
 
 
+#Добавить построковое считывание и вывести в цикле каждое предложение
 def read_sentence_about_py(filename: str):
     with open(filename, 'r') as inpSentence:
         sentence_list = inpSentence.read()
@@ -30,33 +31,29 @@ def read_sentence_about_py(filename: str):
 
 
 def greeting_to_file(filename: str):
-    name = 'Name'
-    while name:
-        name = str(input('Enter name for greeting in file: '))
-        if not name:
-            break
-        with open(filename, 'a') as hello:
+    name = None
+    with open(filename, 'a') as hello:
+        while not name:
+            name = input('Enter name for greeting in file: ')
             hello.write('Hi,' + name + '\n')
-        print('Hi,', name)
-    return 'Completed'
+            print('Hi,', name)
 
 
 def the_in_text(filename: str):
     with open(filename, 'r') as book:
-        string_book = book.read()
-    return sum(string_book.count(item) for item in ['The', 'the'])
+        return book.read().lower().count('the')
 
 
 def book(filename: str, outpfilename: str):
-    with open(filename, 'r', encoding='utf8') as book_for_read:
+    with open(filename, 'r') as book_for_read:
         reading_result = book_for_read.read()
 
     with open(outpfilename, 'w', encoding='utf8') as book_for_write:
         book_for_write.write(textwrap.fill((reading_result.replace('\n', '')), width=120))
-    return 'Complete'
 
 
 if __name__ == '__main__':
+    '''
     print(read_and_write('input/numbers.txt', 'output/sum_numbers.txt'))
 
     print(even_or_odd('output/typeOfNumber.txt'))
@@ -69,4 +66,6 @@ if __name__ == '__main__':
 
     print(the_in_text('../../input/book.txt'))
 
+    print(book('../../input/book.txt', '../../output/book_out.txt'))
+    '''
     print(book('../../input/book.txt', '../../output/book_out.txt'))
