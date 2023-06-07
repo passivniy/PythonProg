40aa70bfd1951a1102659ec6c95662dbd5589aa5
passivniy <danik133743@gmail.com>
2023-06-07 10:05:01 +0200

    Fix style code.

--- Lytvynov_Task3.py
+++ Lytvynov_Task3.py
@@ -1,9 +1,9 @@
 import math
-from import_modules.validator import numbers_isnt_negatives
+from import_modules.validator import get_positive_number
 
 print('''Task_3 Danyil
  Lytvynov IKM-221D Task_3 ''')
 
-x = numbers_isnt_negatives()
+x = get_positive_number()
 
 print(f'y = {math.sqrt(math.log(pow(x, 2) - 4))}')
